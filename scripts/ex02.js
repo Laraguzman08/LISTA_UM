d1c0d788718c462b1420b4205f6212ebc3a8619f
laraemiliagb <lara_guzman@estudante.sesisenai.org.br>
2026-03-20 11:33:43 -0300

    add - Ex 02

---
+++ scripts/ex02.js
@@ -0,0 +1,8 @@
+function contagemNasa() {
+    alert("Iniciando a contagem para o lançamento!");
+
+    for(let i = 10; i >= 0; i--) {
+        alert("Contagem regressiva: " + i);
+    }
+    alert("Lançamento realizado!");
+}
