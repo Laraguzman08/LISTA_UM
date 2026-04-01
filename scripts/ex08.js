7d6fefc854739099e0cc8aeb6c4d6bd60b9e7b05
LaraEmiliaGB <lara_guzman@estudante.sesisenai.org.br>
2026-03-31 21:33:32 -0300

    add - Ex 08

---
+++ scripts/ex08.js
@@ -0,0 +1,12 @@
+function duasVariaveis() {
+    let numero1 = 18;
+    let numero2 = 15;
+    let diferenca = numero1 - numero2;
+    let resultado = (numero1 * 2) + (numero2 * 3);
+    let multiplica = numero1 * numero2;
+
+    console.log("Os valores são 18 e 15.");
+    console.log("a) Diferença das duas variaveis: " + diferenca);
+    console.log("b) Dobro da primeira somado ao triplo da segunda: " + resultado);
+    console.log("c) Multiplicação das duas variáveis: " + multiplica);
+}
